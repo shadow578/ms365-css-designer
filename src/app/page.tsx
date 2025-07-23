a21029047944c7847e38f13fb1e8fadb2f4687a0
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-07-23 18:44:28 +0200

    use non-react trpc api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { api } from "~/trpc/server";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
+  const hi = await api.helloWorld.hello({ text: "World" });
+
   return (
     <HydrateClient>
-      <h1> Hello World </h1>
+      <h1>{hi.greeting ?? "Loading..."}</h1>
     </HydrateClient>
   );
 }
