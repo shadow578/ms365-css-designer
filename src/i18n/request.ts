6a2e90a82e236b9e63432edf6f91d6eb421127ef
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-08-03 14:33:24 +0200

    add fallback logic to default locale

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,17 +1,30 @@
 import type { DeepPartial } from "@trpc/server";
 import { getRequestConfig } from "next-intl/server";
 import { getUserLocale } from "~/server/locale";
+import { DEFAULT_LOCALE, LOCALES, type Locale } from "./config";
+import deepmerge from 'deepmerge';
 
 type MessageCatalog = DeepPartial<Record<string, unknown>> | null | undefined;
 
-export default getRequestConfig(async () => {
-  const locale = await getUserLocale();
-
-  const messages = (
+async function loadMessageCatalog(locale: Locale): Promise<MessageCatalog> {
+  return (
     (await import(`../../messages/${locale}.json`)) as {
       default: MessageCatalog;
     }
   ).default;
+}
+
+export default getRequestConfig(async () => {
+  const locale = await getUserLocale();
+
+  let messages: MessageCatalog;
+  if (LOCALES.includes(locale as Locale)) {
+    messages = await loadMessageCatalog(locale as Locale);
+  }
+  if (locale !== DEFAULT_LOCALE) {
+    const fallback = await loadMessageCatalog(DEFAULT_LOCALE);
+    messages = deepmerge(fallback ?? {}, messages ?? {}) as MessageCatalog
+  }
 
   return {
     locale,
