4e1af12b08749c8c91473b41b2c14f8c8e80912f
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-08-04 17:51:47 +0200

    add zod transformation function

--- src/util/zodExtras.ts
+++ src/util/zodExtras.ts
@@ -1,4 +1,10 @@
-import z from "zod";
+import z, {
+  unknown,
+  ZodObject,
+  ZodSchema,
+  ZodType,
+  type ZodRawShape,
+} from "zod";
 
 const SCHEMA = {
   /**
@@ -16,3 +22,51 @@ const SCHEMA = {
       .regex(/^#(?:[0-9A-Fa-f]{8})|(?:[0-9A-Fa-f]{6})|(?:[0-9A-Fa-f]{3})$/),
 };
 export default SCHEMA;
+
+/**
+ * transforms a given object such that it matches the given zod schema, removing non-matching properties.
+ *
+ * @param schema the zod schema to validate against
+ * @param data the data to transform
+ * @returns the transformed data if it matches the schema, or undefined if it does not match
+ */
+export function transform<T extends ZodRawShape>(
+  schema: ZodObject<T>,
+  data: unknown,
+): z.infer<ZodObject<T>> | undefined {
+  if (typeof data !== "object" || data === null) {
+    return undefined;
+  }
+
+  let result = schema.safeParse(data);
+
+  if (result.success) {
+    console.log("all ok", result.data);
+    return result.data;
+  }
+
+  data = Object.fromEntries(
+    Object.entries(data)
+      .map(([key, value]) => {
+        const expectedType = schema.shape[key];
+        if (!expectedType) {
+          return [key, undefined];
+        }
+
+        if (expectedType instanceof ZodObject) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- FIXME wonky
+          value = transform(expectedType, value);
+          console.log("c vv", value);
+        }
+
+        const r = expectedType.safeParse(value);
+        console.log(key, " r ", r);
+
+        return r.success ? [key, r.data as unknown] : [key, undefined];
+      })
+      .filter(([, value]) => value !== undefined),
+  ) as unknown;
+
+  result = schema.safeParse(data);
+  return result.success ? result.data : undefined;
+}
