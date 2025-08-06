bd1417617f2b68181b102e42ecc069d8cde09f7d
Chris <52449218+shadow578@users.noreply.github.com>
2025-08-06 11:34:51 +0000

    fix error type in URL Component

--- src/components/designer/components/controls/Url.tsx
+++ src/components/designer/components/controls/Url.tsx
@@ -1,12 +1,15 @@
 import { Input } from "@chakra-ui/react";
 import type { PropsFor } from ".";
+import { useTranslations } from "next-intl";
 
 export default function URLSelectionControl(props: PropsFor<"url">) {
+  const t = useTranslations("CSSDesigner.controls.URLSelectionControl");
+  
   return (
     <Input
       type="url"
       variant="flushed"
-      placeholder={props.options.displayName}
+      placeholder={t("input.placeholder")}
       value={props.value}
       onChange={(e) => props.onChange(e.target.value)}
     />
