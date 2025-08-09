4632d6dae6f1516f823ed62d832e01c1531ab282
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-08-09 19:20:25 +0200

    update zod code from v3 to v4

--- src/components/designer/definitions/properties.ts
+++ src/components/designer/definitions/properties.ts
@@ -161,7 +161,7 @@ export function assertCSSPropertyValue<
   const { kind, schema } = getSchemaFor(prop);
 
   schema.parse(value, {
-    errorMap: () => ({
+    error: () => ({
       message: `Invalid value for property '${prop}' of kind '${kind}'`,
     }),
   });
