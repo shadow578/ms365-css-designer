8a13d11c5638e7636f0176e1bb90f7e7ed7d1613
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-08-10 14:29:51 +0200

    autoformat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: "400px", // = minWidth of EditorPane 
+  width: "400px", // = minWidth of EditorPane
   initialScale: 1,
 };
 
