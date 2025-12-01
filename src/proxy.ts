014bf2f0727fdefa23be107483ad9f80ede96afd
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-12-01 14:12:54 +0100

    migrate nextjs middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const url = new URL(req.url);
 
   if (url.pathname.startsWith("/converged-signin-page")) {
